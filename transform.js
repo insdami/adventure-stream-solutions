e7c1c63ec51413e4388123492db88b4d7b822795
damian.albrun <damian.albrun@globallogic.com>
2015-01-13 14:54:00 -0300

    Transform's leasson solution

--- transform.js
+++ transform.js
@@ -1,2 +1,6 @@
-var map = require('through');
+var through = require('through');
+
+process.stdin.pipe(through(function (buf) {
+	this.queue(buf.toString().toUpperCase());
+})).pipe(process.stdout);
 
